0b83c9b3140e0aef156c73215e628357d8a62a78
Brayden H <35241666+hord-brayden@users.noreply.github.com>
2018-12-04 12:26:20 -0800

    Update HuntTest.java

--- 14 Topics/Afghan-Trail-master/Afghan_Trail/test/byui/cit260/afghan_trail/controller/HuntTest.java
+++ 14 Topics/Afghan-Trail-master/Afghan_Trail/test/byui/cit260/afghan_trail/controller/HuntTest.java
@@ -14,8 +14,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
- * @author jonsi
+ In this class, we are using many of the JUnit testing classes and tests. Specifically, by importing the Before, After, 
+ and Test features, we are able to simulate the tests remotely from within the controller in this example, and it also 
+ provides other functionality such as Setup and tearDown constructors. 
+ 
+ The Junit.After declared in the superclass should run after whatever the current classes tests are, unless there is an
+ Override somewhere to call it in.
+ 
+ As opposed to the junit.After import, the .Before class runs before each individual test. This is similar to the 
+ .BeforeClass, however the Beforeclass runs only once before the entire test fixture, I have heard some call it. Or it
+ can be referred to as running before the entire test series/sequence.
+ 
+ 
+ 
  */
 public class HuntTest {
     
@@ -39,19 +50,20 @@ public class HuntTest {
         
         //declare variables for testing
         //initialize variables for first test
+        //The duck item is just in place of some of the other animals that can be aquired.
         long stamina = 70;
         int chance = 30;
         double price = 4.00;
         BigDecimal priceBD = new BigDecimal(price);
         Item expResults = new Item("Duck", 1, priceBD);
-        
+//Case 1       
         System.out.println("\tTest case 1");
         Item result = Hunt.tryHunt(stamina, chance, price);
         if (result != null)
             System.out.println(result.toString());
         assertEquals(expResults, result);
         System.out.println(!(expResults == result));
-        
+//Case 2        
         System.out.println("\tTest case 2");
         stamina = -10;
         chance = 4;
@@ -63,7 +75,7 @@ public class HuntTest {
             System.out.println(result.toString());
         assertEquals(expResults, result);
         System.out.println(!(expResults == result));
-        
+//Case 3        
         System.out.println("\tTest case 3");
         stamina = 20;
         chance = -5;
@@ -75,7 +87,7 @@ public class HuntTest {
             System.out.println(result.toString());
         assertEquals(expResults, result);
         System.out.println(!(expResults == result));
-        
+//Case 4        
         System.out.println("\tTest case 4");
         stamina = 45;
         chance = 60;
@@ -87,7 +99,7 @@ public class HuntTest {
             System.out.println(result.toString());
         assertEquals(expResults, result);
         System.out.println(!(expResults == result));
-        
+//Case 5
         System.out.println("\tTest case 5");
         stamina = 67;
         chance = 20;
@@ -99,7 +111,7 @@ public class HuntTest {
             System.out.println(result.toString());
         assertEquals(expResults, result);
         System.out.println(!(expResults == result));
-
+//Case 6
         System.out.println("\tTest case 6");
         stamina = 0;
         chance = 0;
